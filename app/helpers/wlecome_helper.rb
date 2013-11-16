80c7cb92da73ccbe3dcffe60436a79a0dd9e97ad
shan <corallishan@gmail.com>
2013-11-16 14:39:30 -0500

    Created welcom controller with index and about views.

---
+++ app/helpers/wlecome_helper.rb
@@ -0,0 +1,2 @@
+module WlecomeHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Table::Application.routes.draw do
+  
+  get "welcome/index"
+  get "welcome/about"
+
+  root to: 'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
